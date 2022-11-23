baadb94e3d9c6413937f9d01f423dfa48c09af11
tito-k <31657846+tito-k@users.noreply.github.com>
2022-11-23 01:20:14 +0100

    chore: add error message if no file is uploaded

--- src/controllers/file/file.controller.ts
+++ src/controllers/file/file.controller.ts
@@ -4,14 +4,16 @@ import {
   NextFunction,
 } from 'express';
 import serverConfig from '../../config/sever.config';
+import { BadRequestError } from '../../errors';
 
 export default class FileController {
   protected async create(req: Request, res: Response, next: NextFunction): Promise<Response> {
     try {
-      const { file: { path } } = req;
+      const { file } = req;
+      if (!file) throw new BadRequestError('No file uploaded.');
       return res.status(201).json({
         message: 'File uploaded successfully.',
-        data: path,
+        data: file.path,
       });
     } catch (error) {
       serverConfig.DEBUG(`Error in file create controller method: ${error}`);
