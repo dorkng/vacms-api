fc7df7150df34427955d53b1acb0ae2c6381a20a
tito-k <31657846+tito-k@users.noreply.github.com>
2022-12-03 23:40:15 +0100

    refactor: only allow pdf files to be uploaded

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -23,8 +23,8 @@ class FileService {
   });
 
   private fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback): void => {
-    if (file.mimetype.includes('csv')) {
-      cb(null, false);
+    if (!file.mimetype.includes('pdf')) {
+      cb(new BadRequestError('Invalid file type.'));
     }
     cb(null, true);
   };
